fc9a0b7fc6bfb663bd93f5309a0f592467e7fb95
nathuuy <nathalia.cericatto@gmail.com>
2026-04-22 20:52:53 -0300

    inicio projeto da aula

---
+++ projetoQuarta/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <PostCard :posts="posts" />
+  </div>
+</template>
+
+<script>
+import PostCard from './components/PostCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PostCard
+  },
+  data() {
+    return {
+      posts: []
+    }
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((dados) => {
+        this.posts = dados.slice(0, 10) 
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ projetoQuarta/src/components/PostCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-for="post in posts" :key="post.id" class="card">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: Array
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
